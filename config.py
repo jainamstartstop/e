847287c5b06b887e285d446743d31e723caea50c
jainamstartstop <mazziniboy41@gmail.com>
2024-10-25 02:03:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7763566676:AAFPUFSFHkdnPppDy5ov6tumAsWuTcgltUU")
+BOT_TOKEN=("7236650621:AAH4MJC2Pd6mNXIrmBK-Gqrs8aKtZknuqNw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Mr_sid_001")
+OWNER_USERNAME=("@God_x_Sikandar")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1329951770")
+ADMIN_IDS=("1216702307")
 
 
 
